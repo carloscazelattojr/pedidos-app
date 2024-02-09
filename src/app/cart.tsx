a325a63d6a6dd6c4f6cde33db3360139f6943d9c
Carlos Cazelatto Jr <carlosjunior1983@gmail.com>
2024-02-09 00:52:53 -0300

    Update cart.tsx

--- src/app/cart.tsx
+++ src/app/cart.tsx
@@ -11,7 +11,7 @@ import { useState } from "react";
 import { Alert, Linking, ScrollView, Text, View } from "react-native";
 import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
 
-const PHONE_NUMBER = "5544999513420";
+const PHONE_NUMBER = "554499999999";
 
 export default function Cart() {
     const navigation = useNavigation();
